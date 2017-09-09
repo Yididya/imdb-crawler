cf10cbf21a760a71609a0509a9491a7aec62da95
Yididya <yididiya.yilma@gmail.com>
2017-09-09 13:47:00 +0300

    Move URLs, db settings to settings.py

--- crawler/Crawler.py
+++ crawler/Crawler.py
@@ -1,35 +1,33 @@
 import urllib
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
+
 from crawler.Repository import Repository
 from crawler.model.Movie import Movie
-
-URL_TOP_250 = 'http://www.imdb.com/chart/top'
-URL_BOTTOM_100 = 'http://www.imdb.com/chart/bottom'
-ROOT_URL =  'http://www.imdb.com'
-NUMBER_OF_THREADS = 10
+from crawler.settings import ROOT_URL, URL_BOTTOM_100, URL_TOP_250, NUMBER_OF_THREADS
 
 def retrieveAndSaveMovieData(url):
     movie = Movie(ROOT_URL + url)
     repository.saveMovie(movie)
 
 def retrieveMovieList(url):
-    response = urllib.request.urlopen(url)
+    print('Retriving movie Lists....')
+    
+    response = urllib.urlopen(url)
     html = response.read()
-    soup = BeautifulSoup(html)
-
-    movieList = soup.table.find_all("a")
-
+    soup = BeautifulSoup(html, 'html.parser')
+    
     p = Pool(NUMBER_OF_THREADS)
 
-    movieListUrl = []
-    for movie in movieList:
-        movieListUrl.append(movie['href'])
-    # To debug, uncomment the following line...
-    #    retrieveAndSaveMovieData(movie['href'])
+    print('Fetching individual movie data...\nThis might take a while. Go get your coffee!')
 
-    # ... and comment the following line.
-    p.map(retrieveAndSaveMovieData, movieListUrl)
+    movie_url_list = set(map(lambda movie: movie['href'], soup.table.find_all('a')))
+    p.map(retrieveAndSaveMovieData, movie_url_list)
+    
+    p.close()
+    p.join() # Wait for the workers to die.
+    print('Voila!! It is all done now.')
+
 
 repository = Repository()
 retrieveMovieList(URL_TOP_250)
